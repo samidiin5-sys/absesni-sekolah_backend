eef9b4db3708f1153ac0da84c6143d3af91c830c
muhamadhids <muhamadhids@gmail.com>
2026-07-22 13:22:16 +0700

    debug: catch top level module load errors in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
-const app = require('../app');
+let app;
+try {
+  app = require('../app');
+} catch (error) {
+  app = (req, res) => {
+    res.status(500).send("MODULE LOAD CRASHED: " + error.message + "\n" + error.stack);
+  };
+}
 
 module.exports = app;
